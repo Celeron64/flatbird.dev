3528cf6244b6137bb89eb94d0d41f083143d295a
Flatbird <official.flatbird@gmail.com>
2019-12-14 13:38:17 -0800

    My first Nuxt project

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="NuxtLogo" src="bird.png" />
+</template>
+<style>
+.NuxtLogo {
+  animation: 5s appear;
+  border-radius: 50%;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  background: #2c2f33;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--yellow {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #fff1a3;
+  color: #fff1a3;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--yellow:hover {
+  color: #2c2f33;
+  background-color: #fff1a3;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #99aab5;
+  color: #99aab5;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #99aab5;
+}
+
+.button--66ccff {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #66ccff;
+  color: #66ccff;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--66ccff:hover {
+  color: #fff;
+  background-color: #66ccff;
+}
+</style>
